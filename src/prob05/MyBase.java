6f068cc886e1f5396d17637b04be73b02a561a6a
bit-user <bit-user@bit>
2017-04-25 18:33:54 +0900

    MyBase.java Prob05.java

--- src/prob05/MyBase.java
+++ src/prob05/MyBase.java
@@ -10,22 +10,19 @@ public class MyBase extends Base {
 	
 
 	@Override
-	public void service(String state) {
-		if( state.equals( "낮" ) ) {
-			day();
-		}
-		else if(state.equals( "오후" ))
+	public void service(String state) {	
+		 if(state.equals( "오후" ))
 		{
-			night();
+			afternoon();
 		}
 		else
-			super.night();
+			super.service(state);
 	}
 
 
 
-	@Override
-	public void night() {
+
+	public void afternoon() {
 		System.out.println("오후도 낮과 마찬가지로 일해야 합니다.");
 	}   	
 	
